636613daabe6e9fe99bf1ac9f0f8ac86ed1cc657
vidyadhar01 <122983877+vidyadhar01@users.noreply.github.com>
2023-11-19 11:31:57 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Intersection of two sorted Linked lists/intersection-of-two-sorted-linked-lists.cpp
@@ -0,0 +1,118 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    if(n)
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    else cout<< " ";
+}
+
+
+// } Driver Code Ends
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    Node* findIntersection(Node* head1, Node* head2)
+    {
+        // code goes here.
+        Node* head= new Node(-1);
+        Node* ans = head;
+        
+        Node* A=head1;
+        Node* B=head2;
+        
+        while(A && B){
+            if(A->data == B->data){
+                ans->next = new Node(A->data);
+                ans = ans->next;
+                A = A->next;
+                B = B->next;
+            }
+            else if(A->data > B->data){
+                B = B->next;
+            }
+            else{
+                A = A->next;
+            }
+        }
+        return head->next;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Solution ob;
+	    Node* result = ob.findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
