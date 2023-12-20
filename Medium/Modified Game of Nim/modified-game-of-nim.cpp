7b8e0665926cf5caf168530f252185b5ca479b09
vidyadhar01 <122983877+vidyadhar01@users.noreply.github.com>
2023-12-20 19:52:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Modified Game of Nim/modified-game-of-nim.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int findWinner(int n, int A[]){
+        // code here
+        int xorr = 0;
+        for (int i = 0; i < n; i++) {
+            xorr ^= A[i];
+        }
+
+        if (xorr == 0) {
+            return 1; // Player 1 wins
+        } 
+        else {
+             if(n%2==0){
+                 return 1;
+             }
+             else{
+                 return 2;
+             }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int A[n];
+        for(int i = 0;i < n;i++)
+            cin>>A[i];
+        
+        Solution ob;
+        cout<<ob.findWinner(n, A)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
