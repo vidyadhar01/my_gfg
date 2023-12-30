fc1b60985e58231344ed2111d24b4241efda0bc6
vidyadhar01 <122983877+vidyadhar01@users.noreply.github.com>
2023-12-30 13:21:29 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Winner of an election/winner-of-an-election.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <cstdlib>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+  
+    //Function to return the name of candidate that received maximum votes.
+    vector<string> winner(string arr[],int n)
+    {
+        // Your code here
+        // Return the string containing the name and an integer
+        // representing the number of votes the winning candidate got
+        map<string,int> mp;
+        
+        for(int i=0 ; i<n; i++){
+            mp[arr[i]]++;
+        }
+        
+        vector<string> ans;
+        int maxi= 0;
+        string temp;
+        
+        for(auto it: mp){
+            if(it.second>maxi){
+                maxi= it.second;
+                temp=it.first;
+            }
+        }
+        ans.push_back(temp);
+        ans.push_back(to_string(maxi));
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    for(int i=0;i<t;i++)
+    {
+        
+        
+        int n;
+        cin>>n;
+        
+        string arr[n];
+        
+        for (int i=0;i<n;i++)
+        cin>>arr[i];
+        Solution obj;
+        vector<string> result = obj.winner(arr,n);
+        
+        cout<<result[0] << " " << result[1] << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
